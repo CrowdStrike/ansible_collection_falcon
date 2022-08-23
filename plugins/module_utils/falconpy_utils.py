6dd74972053f33ea1dfe6cc17ce70d459104d394
Carlos Matos <carlos.matos@crowdstrike.com>
2022-08-23 14:22:22 -0400

    Stub for falconpy helper utils + doc updates

---
+++ plugins/module_utils/falconpy_utils.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Common helper functions for FalconPy modules.
+# Copyright: (c) 2021, CrowdStrike Inc.
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Make coding more python3-ish
