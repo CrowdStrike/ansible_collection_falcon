3c681861f7d6bbddea36665dfdfa46638ae5ade4
Carlos Matos <matosc15@gmail.com>
2023-08-21 15:26:39 -0400

    FalconPy 1.3 Scaffolding (#384)
    - Include FalconPy library for enhanced functionality
    - Develop the initial Ansible module to support our project's needs
    - Add helper functions to streamline processes
    - Implement authentication support with access tokens
    
    This commit marks a significant step in our project, laying the foundation for future development and integration with CrowdStrike Falcon API.

---
+++ plugins/module_utils/version.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+# Make sure to keep this file in sync with the version in the galaxy.yml
+__version__ = "4.0.0"
+__author__ = "CrowdStrike"
+__email__ = "cloud-integrations@crowdstrike.com"
