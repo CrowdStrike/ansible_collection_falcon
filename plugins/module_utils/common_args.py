3ad6eeec86e2f1b58516f91959ca5db51e52d6c3
Carlos Matos <carlos.matos@crowdstrike.com>
2023-08-25 12:38:40 -0400

    feat(cid_info): added new cid_info module
    Also renamed args_common to common_args

--- plugins/module_utils/common_args.py
+++ plugins/module_utils/common_args.py
@@ -1,8 +1,7 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Common helper functions for FalconPy modules.
-# Copyright: (c) 2021, CrowdStrike Inc.
-#
+# Copyright: (c) 2023, CrowdStrike Inc.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
