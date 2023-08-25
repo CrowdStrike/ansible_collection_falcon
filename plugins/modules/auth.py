4151df5e593404714a2b0c7b8e2da8900f804d81
Carlos Matos <carlos.matos@crowdstrike.com>
2023-08-25 12:40:01 -0400

    chore: update copyright header on auth module

--- plugins/modules/auth.py
+++ plugins/modules/auth.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Ansible info module used to query options for the CrowdStrike Falcon Sensor on Linux systems.
-# Copyright: (c) 2021, CrowdStrike Inc.
-#
+# Copyright: (c) 2023, CrowdStrike Inc.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -92,7 +90,7 @@ auth:
 import traceback
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
-from ansible_collections.crowdstrike.falcon.plugins.module_utils.args_common import (
+from ansible_collections.crowdstrike.falcon.plugins.module_utils.common_args import (
     falconpy_arg_spec,
 )
 from ansible_collections.crowdstrike.falcon.plugins.module_utils.falconpy_utils import (
