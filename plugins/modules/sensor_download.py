0eef1e16de9bfe550399aa5ae888016e2fba87fa
Carlos Matos <carlos.matos@crowdstrike.com>
2023-08-28 08:14:57 -0400

    feat(sensor_download): adding initial scaffold for new sensor_download module

---
+++ plugins/modules/sensor_download.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2023, CrowdStrike Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: sensor_download
+
+short_description: Download Falcon Sensor Installer
+
+version_added: "4.0.0"
+
+description:
+  - Download the Falcon Sensor Installer by SHA256 ID.
+"""
