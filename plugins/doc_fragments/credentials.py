5ddc320355d7e3d25961cf3f0a9ded3ee637af80
Carlos Matos <carlos.matos@crowdstrike.com>
2023-09-01 15:13:57 -0400

    chore: update copyright header for doc fragments

--- plugins/doc_fragments/credentials.py
+++ plugins/doc_fragments/credentials.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2017,  Ansible Project
+# Copyright: (c) 2023, CrowdStrike Inc.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
